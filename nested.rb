3ad015c09e7c7dc21fdade2c0449df53300eb69d
Todd Carlson <todd.carlson3030@me.com>
2019-12-18 20:00:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,11 +85,17 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+     
+programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
+return programmer_hash
+
 
     
 end
 
 def changing_alan
+  alans_new_info = "GUI"
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -101,6 +113,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
      
 end
@@ -121,5 +136,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     return programmer_hash
 
 end
